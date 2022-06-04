ca96896f3309dfe42bf9c799849468e6affd8fd5
ljanotte.dev <ljanotte-dev@gmail.com>
2022-06-03 23:07:48 -0300

    DTO

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -35,12 +35,12 @@ public class Category implements Serializable {
 		this.id = id;
 	}
 
-	public String getNome() {
+	public String getName() {
 		return name;
 	}
 
-	public void setNome(String nome) {
-		this.name = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	@Override
